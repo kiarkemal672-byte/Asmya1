5267f9aa252c66b2d08637dd85c18a74ea0079b8
kiarkemal672-byte <kiarkemal672@gmail.com>
2026-07-31 14:09:14 +0300

    Create app_constants.dart

---
+++ frontend/lib/core/constants/app_constants.dart
@@ -0,0 +1,9 @@
+class AppConstants {
+  static const String appName = 'ASMYA';
+  static const String appNameFull = 'Abubeker Siddiq Masjid Youth Association';
+  static const String arabicName = 'مسجد أبوبكر الصديق - رابطة الشباب';
+  static const String greeting = 'السلام عليكم ورحمة الله وبركاته';
+  static const String footerBasmala = 'In the name of Allah, the Most Gracious, the Most Merciful';
+  static const String apiBaseUrl = 'http://10.0.2.2:5000/api';
+  static const Duration apiTimeout = Duration(seconds: 20);
+}
